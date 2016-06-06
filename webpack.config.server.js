efa01c31a846f6c1631f09af38f6bc92e21b7a1b
solpie <solpie.net@gmail.com>
2016-06-06 18:07:49 +0800

    clean

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -2,22 +2,17 @@ var nodeExternals = require('webpack-node-externals');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var path = require('path');
 module.exports = {
-    // This is the "main" file which should include all other modules
     entry: {
-        // "server": "./app/server/server.js"
         "main": "./src/main.ts",//electron default entry index.js if no package.json
-        "Server": "./src/Server.ts",//electron default entry index.js if no package.json
+        "Server": "./src/Server.ts"
     },
     target: "electron",
     externals: [nodeExternals()],
     output: {
-        // To the `dist` folder
         path: './app',
-        // With the filename `build.js` so it's dist/build.js
         filename: "[name].js"
     },
     resolve: {
-        // Add `.ts` and `.tsx` as a resolvable extension.
         extensions: ["", ".ts", ".tsx", ".js"]
     },
     devtool: 'source-map',
@@ -40,7 +35,7 @@ module.exports = {
                 from: 'view/**/*.ejs'
             },
             {context: '.', from: 'static/**/*'},
-            {context: '.', from: 'db/**/*'},
+            // {context: '.', from: 'db/**/*'},
             {from: 'src/reload.html'}
         ])
     ]
