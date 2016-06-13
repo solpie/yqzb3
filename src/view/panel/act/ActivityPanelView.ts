4efba44c7fd96ff0e81d1d1497a7f0b00928327c
solpie <solpie.net@gmail.com>
2016-06-13 23:22:30 +0800

    act button

--- src/view/panel/act/ActivityPanelView.ts
+++ src/view/panel/act/ActivityPanelView.ts
@@ -16,4 +16,27 @@ export class ActivityPanelView extends BasePanelView {
     ready() {
         var io = super.ready(PanelId.actPanel);
     }
+    onStartGame() {
+        console.log('onStartGame')
+
+    }
+    onResetGame() {
+        console.log('onResetGame')
+
+    }
+    onRankIn() {
+        console.log('onRankIn')
+
+    }
+    onRankOut() {
+        console.log('onRankOut')
+
+    }
+    onActivityIn() {
+        console.log('onActivityIn')
+
+    }
+    onActivityOut() {
+        console.log('onActivityOut')
+    }
 }
